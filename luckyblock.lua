4c579931f1c1cc5cf8f606963976c3ed8ec495a0
bacaman23 <144810321+bacaman23@users.noreply.github.com>
2023-12-07 03:37:13 +0000

    Update luckyblock.lua

--- luckyblock.lua
+++ luckyblock.lua
@@ -1,3 +1,16 @@
-while wait(0.5) do
-  game:GetService("ReplicatedStorage").SpawnLuckyBlock:FireServer()
+local toggle = false
+
+game.Players.LocalPlayer.Chatted:Connect(function(msg)
+    if msg == "/e start" then
+        toggle = true
+    elseif msg == "/e stop" then
+        toggle = false
+    end
+end)
+
+while wait() do
+    if toggle then
+        game:GetService("ReplicatedStorage").SpawnLuckyBlock:FireServer()
+    end
 end
+
