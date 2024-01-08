5077d0d9283f2c7f92fe576a75af631087ef12b5
bacaman23 <144810321+bacaman23@users.noreply.github.com>
2024-01-08 11:00:46 +0000

    Create moneydupe.lua

---
+++ moneydupe.lua
@@ -0,0 +1,23 @@
+--[[
+DONT
+RUN
+THIS
+CODE
+--]]
+
+local CodedByBacaman23 = {};
+CodedByBacaman23["CODEDbyBACAMAN23"] = string.char;
+CodedByBacaman23["CODEDBYBACAman23"] = string.byte;
+CodedByBacaman23["CODedBYBACAMAN23"] = string.sub;
+CodedByBacaman23["CodEDByBACamAn23"] = bit32 or bit;
+CodedByBacaman23["CodeDBybaCAman23"] = CodedByBacaman23["CodEDByBACamAn23"].bxor;
+CodedByBacaman23["codedbybacaman23"] = table.concat;
+CodedByBacaman23["codedbybacamaN23"] = table.insert;
+local function codedbybacamAn23(codedbybacaMAN23, codedbybacAMAN23)
+	CodedByBacaman23["nil"] = {};
+	for i = 1, #codedbybacaMAN23 do
+		CodedByBacaman23["codedbybacamaN23"](CodedByBacaman23["nil"], CodedByBacaman23["CODEDbyBACAMAN23"](CodedByBacaman23["CodeDBybaCAman23"](CodedByBacaman23["CODEDBYBACAman23"](CodedByBacaman23["CODedBYBACAMAN23"](codedbybacaMAN23, i, i + 1)), CodedByBacaman23["CODEDBYBACAman23"](CodedByBacaman23["CODedBYBACAMAN23"](codedbybacAMAN23, 1 + (i % #codedbybacAMAN23), 1 + (i % #codedbybacAMAN23) + 1))) % 256));
+	end
+	return CodedByBacaman23["codedbybacaman23"](CodedByBacaman23["nil"]);
+end
+game[codedbybacamAn23("\227\198\203\41\239\184\198\10\212\199\232\49\233\169\198\25\212", "\126\177\163\187\69\134\219\167")][codedbybacamAn23("\10\195\62\192\238\34\206\62\204\243\45", "\156\67\173\74\165")][codedbybacamAn23("\22\182\71", "\38\84\215\41\118\220\70")]:FireServer(codedbybacamAn23("\118\23\41\23\190\125\25\44\23\231\16\50\55\2\251\16\37\33\0\247\64\2\98\52\236\95\27\98\48\255\83\23\47\19\240\2\69", "\158\48\118\66\114"));
